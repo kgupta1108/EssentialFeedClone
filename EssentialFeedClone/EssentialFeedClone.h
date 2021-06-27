e033232c43e6fa3053147a264b2b32f6ae0d8f3d
kshitij <kshitij.gupta@swiggy.in>
2021-06-27 10:03:46 +0530

    Initial Commit

---
+++ EssentialFeedClone/EssentialFeedClone.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeedClone.h
+//  EssentialFeedClone
+//
+//  Created by kshitij gupta on 27/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedClone.
+FOUNDATION_EXPORT double EssentialFeedCloneVersionNumber;
+
+//! Project version string for EssentialFeedClone.
+FOUNDATION_EXPORT const unsigned char EssentialFeedCloneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedClone/PublicHeader.h>
+
+
