1b17c667443234f3b242014c0e79e6b8ffe2c2e0
kgupta1108 <kshitij.gupta@swiggy.in>
2021-07-14 21:30:54 +0530

    Does not delete cache upon Local Feed Loader Creation

---
+++ EssentialFeedCloneTests/Feed Cache/CacheFeedUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  CacheFeedUseCase.swift
+//  EssentialFeedCloneTests
+//
+//  Created by kshitij gupta on 14/07/21.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCase: XCTestCase {
+    func test_init_doesNotdeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+}
